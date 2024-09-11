b63d457a4fe6ae907177bbc29860778beba600b6
AkbarFauzy <akbarfa625@gmail.com>
2024-09-11 20:20:58 +0700

    Build

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "4Ruu-Galaxy FuelUp-0.1";
 const contentToCache = [
     "Build/WebGlBuild.loader.js",
-    "Build/WebGlBuild.framework.js.gz",
-    "Build/WebGlBuild.data.gz",
-    "Build/WebGlBuild.wasm.gz",
+    "Build/WebGlBuild.framework.js",
+    "Build/WebGlBuild.data",
+    "Build/WebGlBuild.wasm",
     "TemplateData/style.css"
 
 ];
